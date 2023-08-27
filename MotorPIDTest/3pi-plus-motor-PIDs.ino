7adbb4f9eff6d6cd38a6361ced3888edf30a7835
Robin Sayers <rksayers92@gmail.com>
2023-08-27 16:09:10 +1000

    Added PID test program

---
+++ MotorPIDTest/3pi-plus-motor-PIDs.ino
@@ -0,0 +1,113 @@
+#include <PID_v1.h>
+
+// Timer3 interrupt service routine (this runs at the periodicity defined in MOTOR_CTRL_LOOP_PERIOD_SEC)
+ISR (TIMER3_COMPA_vect) {
+  // Toggle LED to indicate the ISR is running
+  toggleBuiltInLED();
+
+  // Manage the motor PID control
+  motorControlLoop();
+}
+
+void initTimer3() {
+  cli();
+  TCCR3A = 0;
+  TCCR3B = 0;
+  TCNT3 = 0;
+  OCR3A = 10000;
+  TCCR3B = bit(WGM32) | bit(CS32);  // WGM32 => CTC(Clear Timer on Compare Match), CS32 => prescaler 1/256
+  TIMSK3 = bit(OCIE3A);             // OCIE3A => Timer3 compare match A interrupt
+  sei();
+}
+
+void setTimer3(float _timePeriod) {
+  long cnt = 16000000 / 256 * _timePeriod;  // cnt = clk / prescaler * time(s)
+  if(cnt > 65535) {
+    cnt = 65535;        // "timer3 16bit counter over."
+  }
+  OCR3A = cnt;          // Output Compare Register Timer3A
+  TIMSK3 = bit(OCIE3A);
+}
+
+void stopTimer3(){
+    TIMSK3 = 0;
+}
+
+void restartTimer3(){
+  TIMSK3 = bit(OCIE3A); 
+}
+
+// Variable for keeping track of LED state
+volatile boolean timer3_out = HIGH;
+
+void toggleBuiltInLED(){
+  // flicker LED to indicate the loop is running
+  digitalWrite(LED_BUILTIN, timer3_out);
+  timer3_out = !timer3_out;
+}
+
+
+// Variables for velocity calculation and PID control
+volatile static int16_t L_previous_encoder_count = encoders.getCountsLeft();
+volatile static int16_t R_previous_encoder_count = encoders.getCountsRight();
+int16_t L_current_counts; int16_t R_current_counts;
+int16_t L_count_delta; int16_t R_count_delta;
+volatile double L_current_speed_mm_per_sec = 0; volatile double R_current_speed_mm_per_sec = 0;
+volatile double L_desired_speed_mm_per_sec = 0; volatile double R_desired_speed_mm_per_sec = 0;
+
+volatile double L_PID_controller_output = 0; volatile double R_PID_controller_output = 0;
+volatile double L_motor_output = 0; volatile double R_motor_output = 0;
+
+volatile PID LmotorPID(&L_current_speed_mm_per_sec, &L_PID_controller_output, &L_desired_speed_mm_per_sec, P_GAIN, I_GAIN, D_GAIN, DIRECT);
+volatile PID RmotorPID(&R_current_speed_mm_per_sec, &R_PID_controller_output, &R_desired_speed_mm_per_sec, P_GAIN, I_GAIN, D_GAIN, DIRECT);
+volatile int16_t L_feedforward = 0; volatile int16_t R_feedforward = 0;
+
+
+void setupMotorPIDs(){
+  // Use timer 3, as timer 1 is used for something on the 3pi+.
+  initTimer3();
+  
+  // Configure timer 3 to call ISR every x seconds:
+  setTimer3(MOTOR_CTRL_LOOP_PERIOD_SEC);
+  
+  // setup motor PID objects
+  LmotorPID.SetMode(AUTOMATIC); RmotorPID.SetMode(AUTOMATIC);
+  LmotorPID.SetOutputLimits(PID_LOWER_OUTPUT_LIMIT, PID_UPPER_OUTPUT_LIMIT); // by default, this library won't output negative values.
+  RmotorPID.SetOutputLimits(PID_LOWER_OUTPUT_LIMIT, PID_UPPER_OUTPUT_LIMIT);
+
+  // Also set up the LED, will use this to blink indicating that the ISR is running
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void motorControlLoop(){
+  // Compute current speed
+  L_current_counts = encoders.getCountsLeft(); R_current_counts = encoders.getCountsRight();
+  L_count_delta = L_current_counts - L_previous_encoder_count; R_count_delta = R_current_counts - R_previous_encoder_count;
+  L_previous_encoder_count = L_current_counts; R_previous_encoder_count = R_current_counts; // update for next time
+
+  // Take count delta, divide by 358.3 (counts/rev) to get revolutions, then multiply by 100.5 (mm/rotation) to get mm, then divide by time period
+  L_current_speed_mm_per_sec = (double)L_count_delta * 100.5 / (358.3 * MOTOR_CTRL_LOOP_PERIOD_SEC); 
+  R_current_speed_mm_per_sec = (double)R_count_delta * 100.5 / (358.3 * MOTOR_CTRL_LOOP_PERIOD_SEC); 
+  
+  // PID looks at current_speed_mm_per_sec and L_desired_speed_mm_per_sec and sets PID_controller_output
+  LmotorPID.Compute(); RmotorPID.Compute();
+  
+  // Add PID output to feedforward term, set motor command to this value
+  L_motor_output = L_PID_controller_output + L_feedforward; R_motor_output = R_PID_controller_output + R_feedforward;
+  motors.setSpeeds(L_motor_output, R_motor_output);
+}
+
+void setWheelVelsMmPerSec (double L_newSpeed, double R_newSpeed){
+  // disable the timer
+  stopTimer3();
+  
+  // Update desired speed
+  L_desired_speed_mm_per_sec = L_newSpeed; R_desired_speed_mm_per_sec = R_newSpeed;
+  
+  // Update feedforward term
+  L_feedforward = int16_t(L_newSpeed * FEEDFWD_GAIN); R_feedforward = int16_t(R_newSpeed * FEEDFWD_GAIN);
+  
+  // Re-enable the timer
+  restartTimer3();
+}
+
---
+++ MotorPIDTest/MotorPIDTest.ino
@@ -0,0 +1,155 @@
+#include <Pololu3piPlus32U4.h>
+
+using namespace Pololu3piPlus32U4;
+
+Encoders encoders;
+Motors motors;
+ButtonB buttonB;
+OLED display;
+
+// **************************************************************************
+// ********** Tester configurable values below ******************************
+// **************************************************************************
+
+// PID controller settings
+#define MOTOR_CTRL_LOOP_PERIOD_SEC 0.02 // Set the loop time period, in seconds:
+#define P_GAIN 0.00
+#define I_GAIN 0.00
+#define D_GAIN 0.00
+#define FEEDFWD_GAIN (400.0 / 1500.0) // Approximates ideal value by assuming full motor command of 400 corresponds to full linear velocity output of 1500 mm/s
+#define PID_LOWER_OUTPUT_LIMIT -150.0 // Clamp PID controller's output to these values:
+#define PID_UPPER_OUTPUT_LIMIT 150.0
+
+// Step response test:
+const double low_velocity_mm_per_sec = 0;
+const double high_velocity_mm_per_sec = 400;
+const int16_t test_low_duration_ms = 100;
+const int16_t test_high_duration_ms = 400;
+const int16_t test_sample_period_ms = 10;
+
+// **************************************************************************
+// ********** End tester configurable values ********************************
+// **************************************************************************
+
+// Variables for performing the step test:
+const int16_t step_log_length = (test_low_duration_ms + test_high_duration_ms) / test_sample_period_ms;
+int16_t step_response_log[step_log_length] = {};
+
+void performLeftStepInputTest(double low_test_velocity, double high_test_velocity) {
+  // Record start time
+  unsigned long time_to_perform_next_sample_ms = millis();
+  unsigned long high_time_ms = millis() + test_low_duration_ms; // Calculate what time we need to change output value
+  // Initialise iterator for log
+  int i_log = 0;
+
+  // Set left motor to start speed
+  setWheelVelsMmPerSec(low_test_velocity, 0);
+
+  // Start recording left encoder values for the low portion of the test
+  while ((i_log < step_log_length) && (millis() < high_time_ms)) {
+    // Wait for the correct time to take a sample
+    while (millis() < time_to_perform_next_sample_ms) { }
+    // Record encoder value
+    step_response_log[i_log] = encoders.getCountsLeft();
+    // Increment iterator
+    i_log++;
+    // Update this variable to schedule when we'll take the next recording
+    time_to_perform_next_sample_ms += test_sample_period_ms;
+  }
+
+  // Set left motor to high speed (provide the step input)
+  setWheelVelsMmPerSec(high_test_velocity, 0);
+
+  // Start recording left encoder values for the high portion of the test
+  while (i_log < step_log_length) {
+    // Wait for the correct time to take a sample
+    while (millis() < time_to_perform_next_sample_ms) { }
+    // Record encoder value
+    step_response_log[i_log] = encoders.getCountsLeft();
+    // Increment iterator
+    i_log++;
+    // Update this variable to schedule when we'll take the next recording
+    time_to_perform_next_sample_ms += test_sample_period_ms;
+  }
+
+  // Switch off the motor
+  setWheelVelsMmPerSec(0, 0);
+
+  // Print header
+  Serial.print("\n\n");
+  for (int h = 0; h < 40; h++) {Serial.print("-");} Serial.print("\n");
+  Serial.println("LEFT MOTOR PID STEP RESPONSE TEST");
+  for (int h = 0; h < 40; h++) {Serial.print("-");} Serial.print("\n");
+  printLogToSerialPort();
+}
+
+int16_t step_delta;
+float rev_per_sec;
+float mm_per_sec;
+void printLogToSerialPort() {
+  // Print data headers:
+  Serial.print("t(ms)"); Serial.print("\t");
+  Serial.print("steps"); Serial.print("\t");
+  Serial.print("sDelta"); Serial.print("\t");
+  Serial.print("rot/sec"); Serial.print("\t");
+  Serial.print("mm/sec"); Serial.print("\n");
+
+  // Iterate through log, printing each line
+  for (int i = 0; i < step_log_length; i++) {
+    // Generate and print timestamp
+    Serial.print(i * test_sample_period_ms);
+    Serial.print("\t");
+
+    // Print raw data
+    Serial.print(step_response_log[i]);
+    Serial.print("\t");
+
+    // Calculate and print step delta and rotation/sec data
+    // Take count delta, divide by 358.3 (counts/rev) to get revolutions, then divide by time period for rev/sec
+    if (i == 0) {step_delta = 0;} else {step_delta = step_response_log[i] - step_response_log[i-1];}
+    rev_per_sec = (float)step_delta / (358.3 * ((float)test_sample_period_ms / 1000) );
+    Serial.print(step_delta);
+    Serial.print("\t");
+    Serial.print(rev_per_sec);
+    Serial.print("\t");
+
+    // Calculate and print mm/sec data
+    // Multiply by 100.5 (mm/rotation - i.e.: circumference of our wheel) to get mm/sec
+    mm_per_sec = rev_per_sec * 100.5;
+    Serial.print(mm_per_sec);
+    Serial.print("\n");
+  }
+}
+
+void setup()
+{
+  // Set up interrupts and PID controllers
+  setupMotorPIDs();
+
+  // Set up serial link for logging
+  Serial.begin(9600);
+
+  // Set up display and show welcome message
+  display.setLayout11x4();
+  display.gotoXY(0, 0);
+  display.print(F(" MOTOR PID"));
+  display.gotoXY(0, 1);
+  display.print(F("   TEST"));
+  display.gotoXY(0, 2);
+  display.print(F("B -> START"));
+
+  // Wait for B button to be pressed to begin
+  while(!buttonB.getSingleDebouncedPress()) {}
+
+  // Run step response test, print results
+  performLeftStepInputTest(low_velocity_mm_per_sec, high_velocity_mm_per_sec);
+
+  // Run ramp
+}
+
+
+void loop()
+{
+  // Do nothing
+  delay(10000);
+}
